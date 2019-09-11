80eccc6a943815e2c6906814cff7874ce39d5377
Ben Wang <ben.wang@amobee.com>
2019-09-11 00:05:54 -0700

    Adding endpoint to server.js

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ app.use('/', express.static(__dirname + '/public'));
 
 app.listen(3000);
 
+app.get('/test', function (req, res) {
+  res.send({lat: 123, long: 234})
+})
+
+
 console.log("Open 127.0.0.1:3000")
